1e1181dbb5f8009ce9c4a58531480773dbaae310
kiran <v-kpansare@microsoft.com>
2026-09-17 00:17:34 +0530

    refreshing all stuf

--- project01.spec.ts
+++ project01.spec.ts
@@ -2,18 +2,5 @@ import { test, expect } from "@playwright/test";
 
 test("Verify title",async({page})=>{
 
-await page.goto(``);
-await page.locator().click();
-
-await page.goto().click();
-
-//after hii this is the cade at 12:04
-
-//after connecting to imagine let se im able to push it or not :))
-
-/// wahh its connecting viua iamge also gooddddd
-
-
-
 
 })
