8a2877f85536450695c173bc6e542c834a58ab03
kiran <v-kpansare@microsoft.com>
2026-09-17 00:03:37 +0530

    hii

---
+++ project01.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from "@playwright/test";
+
+test("Verify title",async({page})=>{
+
+await page.goto(``);
+await page.locator().click();
+
+await page.goto().click();
+
+
+
+
+
+
+
+
+})
